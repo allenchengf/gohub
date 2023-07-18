be8e583dd255fb197ebe0f7d77fe3a8901782442
andycc77 <andycc77e@gmail.com>
2023-07-19 00:45:09 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello 世界")
+
+	//new一個Gin Engine實例
+	r := gin.New()
+
+	//註冊中間件
+	r.Use(gin.Logger(), gin.Recovery())
+
+	//註冊一個路由
+	r.GET("/", func(c *gin.Context) {
+		//以JSON格式響應
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	//處理404請求
+	r.NoRoute(func(c *gin.Context) {
+		//獲取標頭信息的Accept信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			//如果是HTML的話
+			c.String(http.StatusNotFound, "頁面返回404")
+		} else {
+			//返回JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定義，請確認url和請求方法是否正確",
+			})
+		}
+	})
+
+	// 運行服務,指定port 8000
+	r.Run(":8000")
 }
